897714e5c3845c42319579f770b6af9a126817dd
kiefer <kiefer@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-08-12 21:16:24 +0000

    - ext_emconf.php updated when uploading EXT to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kiwi_gigs/trunk@23333 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "kiwi_gigs"
 #
-# Auto generated 25-07-2009 21:22
+# Auto generated 12-08-2009 23:17
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '0.0.1',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"7540";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"9694";s:14:"ext_tables.php";s:4:"6a00";s:14:"ext_tables.sql";s:4:"e231";s:25:"icon_tx_kiwigigs_main.gif";s:4:"355b";s:16:"locallang_db.xml";s:4:"8796";s:7:"tca.php";s:4:"6dde";s:19:"doc/wizard_form.dat";s:4:"26a3";s:20:"doc/wizard_form.html";s:4:"16dc";s:26:"res/GoogleMapAPI.class.php";s:4:"d3cf";s:23:"res/images/backlink.gif";s:4:"ddf6";s:24:"res/images/bullet_go.gif";s:4:"ed84";s:21:"res/css/kiwi_gigs.css";s:4:"c77d";s:29:"pi2/class.tx_kiwigigs_pi2.php";s:4:"051f";s:17:"pi2/locallang.xml";s:4:"d5fd";s:24:"pi2/static/editorcfg.txt";s:4:"54a0";s:29:"pi1/class.tx_kiwigigs_pi1.php";s:4:"4320";s:22:"pi1/kiwi_gigs_pi1.tmpl";s:4:"fb3f";s:17:"pi1/locallang.xml";s:4:"a0d4";s:24:"pi1/static/editorcfg.txt";s:4:"a78c";s:20:"pi1/static/setup.txt";s:4:"9c90";}',
+	'_md5_values_when_last_written' => 'a:27:{s:9:"ChangeLog";s:4:"7540";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"26d1";s:17:"ext_localconf.php";s:4:"9694";s:14:"ext_tables.php";s:4:"6a00";s:14:"ext_tables.sql";s:4:"e231";s:25:"icon_tx_kiwigigs_main.gif";s:4:"6749";s:16:"locallang_db.xml";s:4:"8796";s:7:"tca.php";s:4:"6dde";s:13:"doc/Thumbs.db";s:4:"3a6f";s:14:"doc/manual.sxw";s:4:"3ee0";s:19:"doc/wizard_form.dat";s:4:"26a3";s:20:"doc/wizard_form.html";s:4:"16dc";s:26:"res/GoogleMapAPI.class.php";s:4:"d3cf";s:23:"res/images/backlink.gif";s:4:"ddf6";s:24:"res/images/bullet_go.gif";s:4:"ed84";s:21:"res/css/kiwi_gigs.css";s:4:"8ab1";s:29:"pi2/class.tx_kiwigigs_pi2.php";s:4:"6901";s:22:"pi2/kiwi_gigs_pi2.tmpl";s:4:"7d79";s:17:"pi2/locallang.xml";s:4:"ec79";s:24:"pi2/static/editorcfg.txt";s:4:"54a0";s:20:"pi2/static/setup.txt";s:4:"4d0b";s:29:"pi1/class.tx_kiwigigs_pi1.php";s:4:"5be7";s:22:"pi1/kiwi_gigs_pi1.tmpl";s:4:"fb3f";s:17:"pi1/locallang.xml";s:4:"a0d4";s:24:"pi1/static/editorcfg.txt";s:4:"a78c";s:20:"pi1/static/setup.txt";s:4:"baa5";}',
+	'suggests' => array(
+	),
 );
 
 ?>
